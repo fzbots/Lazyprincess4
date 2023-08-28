d35d143a9ef0b181a430245662bc4b1058b5a554
fzbots <139735049+fzbots@users.noreply.github.com>
2023-08-28 19:47:48 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -50,12 +50,12 @@ async def start(client, message):
             InlineKeyboardButton('➕↖️ Add Me To Your Groups ↗️➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🧞‍♀️ Search 🧐', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🔔 Updates 🤖', url='https://t.me/Hpbot_update')
+            InlineKeyboardButton('🔔 Updates 🤖', url='https://t.me/Faiz_Movies')
             ],[
             InlineKeyboardButton('🙆🏻 Help 🦾', callback_data='help'),
             InlineKeyboardButton('♥️ About ♥️', callback_data='about')
             ],[
-            InlineKeyboardButton('Watch Tutorial', url='https://youtube.com/@TechnicalHPgamer')
+            InlineKeyboardButton('Main Channel', url='https://t.me/Faiz_Movies')
             ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -79,7 +79,7 @@ async def start(client, message):
             ],
              [
                 InlineKeyboardButton(
-                    "🦋 SUBSCRIBE YT Channel 🦋", url='https://youtube.com/@TechnicalHPgamer'
+                    "🦋 Main Channel 🦋", url='https://t.me/Faiz_Movies'
                 )
             ]
         ]
@@ -103,12 +103,12 @@ async def start(client, message):
             InlineKeyboardButton('➕↖️ Add Me To Your Groups ↗️➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🧞‍♀️ Search 🧐', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🔔 Updates 🤖', url='https://t.me/Hpbot_update')
+            InlineKeyboardButton('🔔 Updates 🤖', url='https://t.me/Faiz_Movies')
             ],[
             InlineKeyboardButton('🙆🏻 Help 🦾', callback_data='help'),
             InlineKeyboardButton('♥️ About ♥️', callback_data='about')
         ],[
-            InlineKeyboardButton('Watch Tutorial', url='https://youtube.com/@TechnicalHPgamer')
+            InlineKeyboardButton('Main Channel', url='https://t.me/Faiz_Movies')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -384,7 +384,7 @@ async def delete_all_index(bot, message):
 @Client.on_callback_query(filters.regex(r'^autofilter_delete'))
 async def delete_all_index_confirm(bot, message):
     await Media.collection.drop()
-    await message.answer('♥️ Thank You MrkillerDeveloper ♥️')
+    await message.answer('♥️ Thank You 𖣘ᴹᴿ๛CRAZYᴮᴼᵞ🕊️⃝🦋 ♥️')
     await message.message.edit('Succesfully Deleted All The Indexed Files.')
 
 
